7790c6d64b26296d13cbccb04a74b4a8ea6a68ea
takanorihozumi <t.h032766@gmail.com>
2018-04-30 16:27:39 +0900

    fix

---
+++ app/models/drama.rb
@@ -0,0 +1,2 @@
+class Drama < ApplicationRecord
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do  
+  resources :impressions
+
+  root :to => 'pages#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",:registrations => 'registrations' }
+
+  resources :users, only: [:show]
+
+  resources :dramas
+
+
+  get 'manage-impression/:id/basics' => 'impressions#basics', as: 'manage_impression_basics'
+end
