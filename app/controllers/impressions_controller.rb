f91b4473b3fabf0a3bc4664477fe46edc61a6e39
takanorihozumi <t.h032766@gmail.com>
2018-05-03 19:22:11 +0900

    use db value

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,2 +1,4 @@
 class ImpressionsController < ApplicationController
+  # loginしてない場合はloginページに飛ばす  
+  before_action :authenticate_user!
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -1,3 +1,4 @@
 class Season < ApplicationRecord
   belongs_to :drama
+  has_many :episodes
 end
