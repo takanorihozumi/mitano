3239561c5e9e1875a25ea6dccf110accf1f45e43
takanorihozumi <t.h032766@gmail.com>
2018-05-06 23:54:27 +0900

    fix episode order

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,6 +2,6 @@ class SeasonsController < ApplicationController
   def show
     @season = Season.order(:id).find(params[:id])
     @drama = Drama.find(@season.drama_id)
-    @episodes =@season.episodes
+    @episodes =@season.episodes.order(:id)
   end
 end
