3fc8e42ab2088ab269f9b3efbad42bfb94059f2d
takanorihozumi <t.h032766@gmail.com>
2018-05-07 20:52:16 +0900

    fix episode bug

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,6 @@
 class EpisodesController < ApplicationController
   def show
     @episode = Episode.find(params[:id])
-    @impressions = Impression.where(episode_id: @episode.id impression_type: "2")
+    @impressions = Impression.where(episode_id: @episode.id, impression_type: "2")
   end
 end
