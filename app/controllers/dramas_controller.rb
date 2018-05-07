9f2700a1e2c94e95a7f733acac7cdb840ffff3e0
takanorihozumi <t.h032766@gmail.com>
2018-05-07 21:59:52 +0900

    fix dramas bug

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -4,7 +4,7 @@ class DramasController < ApplicationController
   # 同じ処理をactionの前で行いたい場合、こんな風に描ける
   # before_action :hogehoge, only: [:index,:show]
   def index
-    @dramas = Drama.all.order(created_at: 'desc')
+    @dramas = Drama.order(created_at: 'desc')
   end
 
   def show
