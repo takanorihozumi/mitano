495e00a104a3bbcf80f980a4d2fa04a62ca413ce
takanorihozumi <t.h032766@gmail.com>
2018-05-08 22:01:25 +0900

    create privacy policy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   def index
     # @users = User.all
   end
+  def privacy_policy
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do  
+  get 'pages/privacy_policy'
   get 'episodes/show'
 
   get 'seasons/show'
