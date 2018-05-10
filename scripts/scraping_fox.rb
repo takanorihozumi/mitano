e4f27492781668eebee5358a6582343944bcb4e1
takanorihozumi <t.h032766@gmail.com>
2018-05-10 21:59:50 +0900

    add initial data column

--- scripts/scraping_fox.rb
+++ scripts/scraping_fox.rb
@@ -10,7 +10,7 @@ class ScrapingFox
     del1 =titles[i].inner_text.delete('第#{zenkaku}話')
     title = del1.delete(zenkaku).delete("0").delete("1").delete("2").delete("3").delete("4").delete("5").delete("6").delete("7").delete("8").delete("9").strip
     content= contents[i].inner_text.strip
-    Episode.create(episode_name: title, over_view: content, season_id: id)
+    Episode.create(episode_name: title,episode_no:i+1, over_view: content, season_id: id)
   end
 end
 end
