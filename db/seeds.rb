d937262a1cffba4fb59a8738f393471055b5dd45
takanorihozumi <t.h032766@gmail.com>
2018-05-20 17:31:31 +0900

    add jurasic world

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
-Drama.create(title: "ニキータ",over_view: "クスリに溺れた少女から殺し屋、そして英雄へ！",image_url: "https://images-na.ssl-images-amazon.com/images/I/81atpjRN7yL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/nikita/intro.php',category: 0)
-Season.create(title: "シーズン1", over_view: "6年前、死刑を宣告されたニキータは、政府の秘密組織ディヴィジョンにリクルートされ、選択の余地なく暗殺者に育て上げられた。", drama_id: 17, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/nikita/ep.php')
-Season.create(title: "シーズン2", over_view: "ブラック・ボックスを手にしたニキータとマイケル、その回収を急ぐディヴィジョンとオーバーサイト。", drama_id: 17, season_no: 2, source_url: 'http://wwws.warnerbros.co.jp/kaidora/nikita/ep.php')
-Season.create(title: "シーズン3", over_view: "ライアン率いる新しいディヴィジョンが活動を開始する。ニキータとマイケルの最初の使命は、逃亡したエージェント、マーティンを捕まえること。", drama_id: 17, season_no: 3, source_url: 'http://wwws.warnerbros.co.jp/kaidora/nikita/ep.php')
-Season.create(title: "シーズン4", over_view: "大統領暗殺の罪を着せられ逃走中のニキータ。ニュースレポーターのデールは、暗殺当日のニキータについて探り始め、ニキータは、彼の質問に答えることで自身の汚名を返上できるのではと期待する。", drama_id: 17, season_no: 4, source_url: 'http://wwws.warnerbros.co.jp/kaidora/nikita/ep.php')
-
+Drama.create(title: "ジュラシック・ニューワールド",over_view: "バンクーバーで、巨大生物によるものと思われる襲撃事件が発生。発明家の主人公エヴァンは、ラジオ電波に何かが干渉していることに気付き、「時空の亀裂」を発見する。なんとそれは、太古の時代と現代をつなぐ扉だった！",image_url: "https://images-na.ssl-images-amazon.com/images/I/915fS9c8NtL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/jurassic_new_world/intro.php',category: 0)
+Season.create(title: "シーズン1", over_view: "不規則的に発生する亀裂から、次々に現れる危険で凶暴な肉食恐竜や、大蛇、昆虫！エヴァンらは極秘に特殊部隊を結成し、恐竜らを無事に元の時空に返し、事故を未然に防ぐべく立ち上がる！しかしそこに忍び寄る政府軍の影ー。", drama_id: 18, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/jurassic_new_world/intro.php')
 
 
