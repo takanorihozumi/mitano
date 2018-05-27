336853d9a067552c0869ac63facdf8d1ad762f21
takanorihozumi <t.h032766@gmail.com>
2018-05-27 12:04:01 +0900

    add draam

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
-Season.create(title: "シーズン2", over_view: "シーズン2は、マイケルたち脱獄犯が当局の追跡をかわすために田舎町を抜けながら逃亡を続けるなか全米規模の追跡捜査が進行するところから幕を開ける。", drama_id: 19, season_no: 2, source_url: 'https://www.netflix.com/jp/title/70140425')
-Season.create(title: "シーズン3", over_view: "再び逮捕され、パナマの刑務所に収監されたマイケル。そこは囚人暴動をきっかけに当局から見放された悪夢のような刑務所だった。", drama_id: 19, season_no: 3, source_url: 'https://www.netflix.com/jp/title/70140425')
-Season.create(title: "シーズン4", over_view: "逃亡中のマイケルとリンカーンは、組織「カンパニー」の打倒を目指すチームの一員となることを強引に引き受けさせられる。", drama_id: 19, season_no: 4, source_url: 'https://www.netflix.com/jp/title/70140425')
-
-Drama.create(title: "ホステージ",over_view: "ジェリー・ブラッカイマー製作総指揮.1日1話で進行する、15日間のタイムリミット・サスペンス！",image_url: "https://images-na.ssl-images-amazon.com/images/I/61OIK7T-STL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/hostages/ep.php',category: 3)
-Season.create(title: "シーズン1", over_view: "1日1話で進行する、15日間のタイムリミット・サスペンス！１シーズン完結！", drama_id: 20, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/hostages/ep.php')
+Drama.create(title: "ヴェロニカ・マーズ",over_view: "「マトリックス」のジョエル・シルバー製作総指揮。全米大人気のクリスティン・ベルが数々の難事件に挑む、新世代ドラマ誕生！私は放課後、探偵になる。",image_url: "https://images-na.ssl-images-amazon.com/images/I/819PYiznqkL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/veronica-mars/ep.php',category: 3)
+Season.create(title: "シーズン1", over_view: "ネプチューン に暮らす高校生のヴェロニカは、私立探偵を始めた父親と新しい生活をスタートさせようとする。", drama_id: 21, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/veronica-mars/ep.php')
+Season.create(title: "シーズン2", over_view: "嵐のような夏を切り抜けたヴェロニカは、探偵を辞めると宣言する。", drama_id: 21, season_no: 2, source_url: 'http://wwws.warnerbros.co.jp/kaidora/veronica-mars/ep.php')
+Season.create(title: "シーズン3", over_view: "大学１年生になったヴェロニカは、授業で行われた“犯人捜しゲーム”でダントツのタイムを記録し、周囲を驚かせる。", drama_id: 21, season_no: 3, source_url: 'http://wwws.warnerbros.co.jp/kaidora/veronica-mars/ep.php')
 
 
