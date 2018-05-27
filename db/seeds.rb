91e15a2e4fd66ac94a7ebd31ae69e7ded90297a4
takanorihozumi <t.h032766@gmail.com>
2018-05-27 20:39:26 +0900

    add drama

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-Drama.create(title: "フォーリングスカイズ",over_view: "スティーヴン・スピルバーグ製作総指揮 x ノア・ワイリー主演の大ヒットサバイバルアクション",image_url: "https://images-na.ssl-images-amazon.com/images/I/91L4NTy4XgL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/fallingskies/ep.php',category: 1)
-Season.create(title: "シーズン1", over_view: "突然、エイリアン（侵略者）による原因不明の言われなき侵略によって、世界は大混乱に見舞われることになる。", drama_id: 22, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/fallingskies/ep.php')
-Season.create(title: "シーズン2", over_view: "マサチューセッツ市民軍第２連隊（セカンド･マス）副隊長のトムが自らの意志でエイリアンの宇宙船に乗り込み、消息を絶ってから３カ月、トムの安否は不明だったが、ウィーバーは以前と同様、セカンド･マスの隊長として隊を率い、アンもまた医師としての立場から生存者の治療に尽力していた。", drama_id: 22, season_no: 2, source_url: 'http://wwws.warnerbros.co.jp/kaidora/fallingskies/ep.php')
-Season.create(title: "シーズン3", over_view: "マサチューセッツ市民軍第２連隊（セカンド・マス）がチャールストンに到着してから７カ月が経ち“新合衆国”大統領に就任したトムは、エイリアンから地球を奪還するための戦いに奔走していた。", drama_id: 22, season_no: 3, source_url: 'http://wwws.warnerbros.co.jp/kaidora/fallingskies/ep.php')
-Season.create(title: "シーズン4", over_view: "トムの一家と戦闘で生き残ったマサチューセッツ市民軍第２連隊（セカンド・マス）の隊員たちはチャールストンを目指して移動を続けていた。", drama_id: 22, season_no: 4, source_url: 'http://wwws.warnerbros.co.jp/kaidora/fallingskies/ep.php')
-Season.create(title: "シーズン5", over_view: "フィフス･シーズンはトムの行方を辿るところから始まる。トムは驚くべき人物に“自分の内なる戦士を捜しだせ”と言われる。", drama_id: 22, season_no: 5, source_url: 'http://wwws.warnerbros.co.jp/kaidora/fallingskies/ep.php')
-
+Drama.create(title: "ストライクバック",over_view: "サリヴァン・ステイプルトン(『300 帝国の進撃』）主演!掟破りの極秘部隊が無謀なミッションに挑む、お子様禁制一大アクションエンターテイメント！",image_url: "https://images-na.ssl-images-amazon.com/images/I/9164um-4pVL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/strikeback/ep.php',category: 0)
+Season.create(title: "シーズン1", over_view: "英軍の極秘部隊“セクション20”のマイケル･ストーンブリッジは、拉致された隊員を救うため、米陸軍特殊部隊“デルタフォース”の元隊員ダミアン･スコットに協力を要請し、非情なテロリスト、ラティーフを追跡する。", drama_id: 23, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/strikeback/ep.php')
 
