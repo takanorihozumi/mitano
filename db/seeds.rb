5981f9fee68e62021e9fac71598883da2dd68194
takanorihozumi <t.h032766@gmail.com>
2018-05-27 21:47:28 +0900

    add 13 reason why

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-Drama.create(title: "ストライクバック",over_view: "サリヴァン・ステイプルトン(『300 帝国の進撃』）主演!掟破りの極秘部隊が無謀なミッションに挑む、お子様禁制一大アクションエンターテイメント！",image_url: "https://images-na.ssl-images-amazon.com/images/I/9164um-4pVL._SY300_.jpg", seasons_count: 0,source_url: 'http://wwws.warnerbros.co.jp/kaidora/strikeback/ep.php',category: 0)
-Season.create(title: "シーズン1", over_view: "英軍の極秘部隊“セクション20”のマイケル･ストーンブリッジは、拉致された隊員を救うため、米陸軍特殊部隊“デルタフォース”の元隊員ダミアン･スコットに協力を要請し、非情なテロリスト、ラティーフを追跡する。", drama_id: 23, season_no: 1, source_url: 'http://wwws.warnerbros.co.jp/kaidora/strikeback/ep.php')
+Drama.create(title: "13の理由",over_view: "ある女子高生が謎の自殺を遂げた後、1人の同級生の元に、彼女が生前に録音した7本のカセットテープが届く。そこには、彼女が死を選んだ理由が語られていた。",image_url: "https://images-na.ssl-images-amazon.com/images/I/41-NQU8Oa2L._SY300_.jpg", seasons_count: 0,source_url: 'https://www.netflix.com/jp/title/80117470',category: 0)
+Season.create(title: "シーズン1", over_view: "ある女子高生が謎の自殺を遂げた後、1人の同級生の元に、彼女が生前に録音した7本のカセットテープが届く。そこには、彼女が死を選んだ理由が語られていた。", drama_id: 24, season_no: 1, source_url: 'https://www.netflix.com/jp/title/80117470')
+Season.create(title: "シーズン2", over_view: "ハンナ・ベイカーはどうして自ら命を絶たなければならなかったのか。人には決して言いたくない秘密を抱える同級生たち。そして驚きの真実が明らかに...。", drama_id: 24, season_no: 2, source_url: 'https://www.netflix.com/jp/title/80117470')
 
