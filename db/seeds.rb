fa63885d12906c389ac55de12a6975d893520415
takanorihozumi <t.h032766@gmail.com>
2018-05-27 22:04:03 +0900

    add new drama

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Drama.create(title: "13の理由",over_view: "ある女子高生が謎の自殺を遂げた後、1人の同級生の元に、彼女が生前に録音した7本のカセットテープが届く。そこには、彼女が死を選んだ理由が語られていた。",image_url: "https://images-na.ssl-images-amazon.com/images/I/41-NQU8Oa2L._SY300_.jpg", seasons_count: 0,source_url: 'https://www.netflix.com/jp/title/80117470',category: 0)
-Season.create(title: "シーズン1", over_view: "ある女子高生が謎の自殺を遂げた後、1人の同級生の元に、彼女が生前に録音した7本のカセットテープが届く。そこには、彼女が死を選んだ理由が語られていた。", drama_id: 24, season_no: 1, source_url: 'https://www.netflix.com/jp/title/80117470')
-Season.create(title: "シーズン2", over_view: "ハンナ・ベイカーはどうして自ら命を絶たなければならなかったのか。人には決して言いたくない秘密を抱える同級生たち。そして驚きの真実が明らかに...。", drama_id: 24, season_no: 2, source_url: 'https://www.netflix.com/jp/title/80117470')
+Drama.create(title: "ストレンジャー・シングス",over_view: "少年の失踪事件や超常パワーを持つ謎の少女の出現など、次々に起こる不可解な事件が、インディアナ州の小さな町ホーキンスに潜む驚愕の秘密を暴いていく。",image_url: "https://images-na.ssl-images-amazon.com/images/I/A1-9PbTL%2BxL._SY300_.jpg", seasons_count: 0,source_url: 'https://www.netflix.com/jp/title/80057281',category: 1)
+Season.create(title: "シーズン1", over_view: "少年の失踪事件や超常パワーを持つ謎の少女の出現など、次々に起こる不可解な事件が、インディアナ州の小さな町ホーキンスに潜む驚愕の秘密を暴いていく。", drama_id: 25, season_no: 1, source_url: 'https://www.netflix.com/jp/title/80057281')
+Season.create(title: "シーズン2", over_view: "姿を消した少年、人目を忍び行われる数々の実験、破壊的な超常現象、突然現れた少女。すべての不可解な謎をつなぐのは、小さな町に隠された恐ろしい秘密", drama_id: 25, season_no: 2, source_url: 'https://www.netflix.com/jp/title/80057281')
 
