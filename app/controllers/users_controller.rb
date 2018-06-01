52b113c6058a73da8de37638747632e77541c33e
takanorihozumi <t.h032766@gmail.com>
2018-06-01 23:30:56 +0900

    fix profile layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  PER = 7
+
   def show 
     @user = User.find(params[:id])
+    @impressions = @user.impressions.page(params[:page]).per(PER)
   end
 end
