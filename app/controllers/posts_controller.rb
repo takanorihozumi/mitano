6f949254cd4b36d5da1c31f1de804070003137da
takanorihozumi <t.h032766@gmail.com>
2018-06-03 18:48:26 +0900

    create matome view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   def index
+    @following = Drama.find(12)
+    @reason = Drama.find(24)
+    @prison = Drama.find(19)
+    @game = Drama.find(6)
   end
 
   def show
