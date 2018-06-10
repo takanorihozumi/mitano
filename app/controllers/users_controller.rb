2a8fb3b5ed3f1356012002a036918ec38f17047a
takanorihozumi <t.h032766@gmail.com>
2018-06-10 18:34:45 +0900

    adjust user profile layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
   end
 
   def following
-    @title = "Following"
+    @title = "をフォロー"
     @user  = User.find(params[:id])
     @users = @user.following
     render 'show_follow'
   end
 
   def followers
-    @title = "Followers"
+    @title = "のフォロワー"
     @user  = User.find(params[:id])
     @users = @user.followers
     render 'show_follow'
