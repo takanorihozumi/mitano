4c1412b6f22df06a61c4003d2b0a6b9cad521640
takanorihozumi <t.h032766@gmail.com>
2018-06-18 22:02:12 +0900

    adjust view

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -4,5 +4,9 @@ class CastsController < ApplicationController
 
   def show
     @cast = Cast.find(params[:id])
+    @dramas =@cast.dramas
+    @dramas.each do |drama|
+      drama.image_url = drama.image_url.to_s.sub!('300', '200')
+    end
   end
 end
