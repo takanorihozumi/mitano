1946191500cccefaeffcf92236813a9d9b992f01
takanorihozumi <t.h032766@gmail.com>
2018-06-20 20:40:34 +0900

    fix

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -1,19 +1,13 @@
 class CastsController < ApplicationController
   def index
-<<<<<<< HEAD
     @casts = Cast.all
-=======
->>>>>>> 8b10a9b5af5d0b274f0c4d88f0e06ed0e67a79a8
   end
 
   def show
     @cast = Cast.find(params[:id])
-<<<<<<< HEAD
     @dramas =@cast.dramas
     @dramas.each do |drama|
       drama.image_url = drama.image_url.to_s.sub!('300', '200')
     end
-=======
->>>>>>> 8b10a9b5af5d0b274f0c4d88f0e06ed0e67a79a8
   end
 end
