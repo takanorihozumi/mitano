ac863b60b75a2aa564054ba67aab331620f9cd09
takanorihozumi <t.h032766@gmail.com>
2018-06-26 20:35:38 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-Drama.create(title: "エメラルドシティ",over_view: "まだ誰も見たことがない驚異の異世界“オズ”で、その覇権をめぐって壮大なバトルが繰り広げられる最新のファンタジーアクション・ドラマが「エメラルドシティ」だ。これまで何度も映像化されてきた名作文学「オズの魔法使い」を下敷きにしながら、魔法使いや魔女が住む“オズ”を誰が支配するかという斬新なミステリー視点を導入。困難な映像化に挑んだのは、『ザ・セル』（2000）『落下の王国』（2006）『白雪姫と鏡の女王』（2012）『セルフレス／覚醒した記憶』（2015）などの映画や多数のＣＭ・ＰＶで、エキゾチックな映像美が世界を圧倒してきた鬼才ターセム・シン監督。シーズン１全10話で監督を務めた。",image_url: "https://images-na.ssl-images-amazon.com/images/I/81P05xMxwiL._SY300_.jpg", seasons_count: 0,source_url: 'http://emerald-city-tv.jp/',category: 1)
-Article.create(title: "【ファンタジー】いま面白い！海外ドラマのファンタジー・SF作品おすすめランキング【2018年】",image: "https://image.tmdb.org/t/p/original/c8aFHYj6vDM1WzymyfAurivJ9Ob.jpg",heading: "ゲームオブスローンズやマジシャンズ、エメラルドシティなど独自の世界観が人気のファンタジードラマ。ファンタジー系海外ドラマの歴代人気ドラマランキングをご紹介します。",trailer: "いかがでしたか？これまで特に人気だった＆面白いオススメの海外ドラマのファンタジー・SF作品をランキング形式でご紹介しました。",created_at: Time.zone.now,updated_at: Time.zone.now)
-
-# ArticleContent.create(article_id: 3,drama_id: 6,title:"",content:"\n",created_at: Time.zone.now,updated_at: Time.zone.now)
-
-
+Cast.create(name:"ジョエリー・リチャードソン",over_view:"ジョエリー・リチャードソン（Joely Richardson）は、イギリス・ロンドン出身の女優である。父親は映画監督のトニー・リチャードソン、母親は女優のヴァネッサ・レッドグレイヴ。姉のナターシャも女優である。幼いころからエキストラとして父親の作品に出ていたが、本人は体操選手になりたかったという。王立演劇学校（RADA）で演技を学び、1985年に映画デビューした。長身で気品のある女優である。物議を醸したテレビ映画『チャタレイ夫人の恋人』やドラマ『NIP/TUCK』でも体当たりの演技を見せている。1992年に映画プロデューサーと結婚し、同年に娘が産まれたが、2001年に別れている。",image_url:"https://image.tmdb.org/t/p/original/fgjpKio6sMD5AxOWQwAdgJQErhm.jpg",movie_id: 20810,birthday:"1965年1月9日")
+DramaCastRelation.create(drama_id:66,cast_id:45)
 # Drama.create(title: "",over_view: "",image_url: "", seasons_count: 0,source_url: '',category: 5)
 # Season.create(title: "シーズン1", over_view: "", drama_id: 43, season_no: 1, source_url: '')
 # Article.create(title: "",image: "",heading: "",trailer: "",created_at: Time.zone.now,updated_at: Time.zone.now)
