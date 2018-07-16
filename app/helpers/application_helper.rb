36c3f926e07fe3951b3bf41de4166b19c5461bf1
takanorihozumi <t.h032766@gmail.com>
2018-07-16 20:01:59 +0900

    fix meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'mitano',
-      title: 'mitano - 海外ドラマの視聴記録が簡単に',
+      title: 'mitano - あなたが見ている海外ドラマの視聴記録を簡単に',
       reverse: true,
       charset: 'utf-8',
-      description: 'ゲームオブスローンズやプリズンブレイクなど、今話題の海外ドラマの視聴記録が簡単に可能になるサービス「mitano」。みんなの視聴リストや感想、レビューを元に、面白い海外ドラマに出会いましょう',
+      description: 'ゲームオブスローンズやプリズンブレイクなど、あなたが見ている海外ドラマの視聴記録が簡単に可能になるサービス「mitano」。みんなの視聴リストや感想、レビューを元に、面白い海外ドラマに出会いましょう',
       keywords: '海外ドラマ,感想,視聴記録',
       canonical: 'http://www.mitano.jp/',
       separator: '|',
@@ -16,7 +16,7 @@ module ApplicationHelper
       og: {
         site_name: 'mitano',
         title: 'mitano - 海外ドラマの視聴記録が簡単に',
-        description: 'ゲームオブスローンズやプリズンブレイクなど、今話題の海外ドラマの視聴記録が簡単に可能になるサービス「mitano」。みんなの視聴リストや感想、レビューを元に、面白い海外ドラマに出会いましょう',
+        description: 'ゲームオブスローンズやプリズンブレイクなど、あなたが見ている海外ドラマの視聴記録が簡単に可能になるサービス「mitano」。みんなの視聴リストや感想、レビューを元に、面白い海外ドラマに出会いましょう',
         type: 'website',
         url: 'http://www.mitano.jp/',
         image: image_url('mitano'),
